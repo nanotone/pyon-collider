3204801424d4601764c6ff8b0fa42f3a4695065a
nanotone <nanotone@quarklet.com>
2014-06-11 11:57:16 -0400

    Move engine stuff to pyon.scengine

--- main.py
+++ main.py
@@ -1,97 +1,16 @@
-import itertools
 import time
 
-from pyon import pool, scsynth
+from pyon import pool, scengine
 import synthdefs
-
-output_rates = {}
-
-nodePool = pool.Pool()
-kbusPool = pool.Pool(0)
-
-
-class Group(object):
-	def __init__(self):
-		self._id = nodePool.get()
-		send('/g_new', self._id, 0, 0)
-
-class KBus(object):
-	def __init__(self, value=None):
-		self._id = kbusPool.get()
-		if value is not None:
-			self.set(value)
-	def set(self, value):
-		sendBundledMsg('/c_set', self._id, value)
-	def __del__(self):
-		kbusPool.put(self._id)
-
-
-class Synth(object):
-	def __init__(self, synthdefName, parent=None, **kwargs):
-		if not parent: parent = default
-		self._id = nodePool.get()
-		if output_rates.get(synthdefName) == 1: # control bus output
-			self._obus = KBus()
-			if 'o' not in kwargs:
-				kwargs['o'] = self._obus
-		with bundle:
-			# setattr ALL the kwargs! But combine the sets and maps for efficiency
-			snew = ['/s_new', synthdefName, self._id, 0, parent._id]
-			mapping = {}
-			for (name, value) in kwargs.iteritems():
-				object.__setattr__(self, name, value)
-				setter = self._set(name, value)
-				if setter:
-					if setter[0] == 'set':
-						snew.extend((name, setter[1]))
-					elif setter[0] == 'map':
-						mapping[name] = setter[1]
-			send(*snew)
-			if mapping:
-				send('/n_map', self._id, *itertools.chain( *mapping.items() ))
-
-	def __setattr__(self, name, value):
-		object.__setattr__(self, name, value)
-		setter = self._set(name, value)
-		if setter:
-			if setter[0] == 'set':
-				send('/n_set', self._id, name, setter[1])
-			elif setter[0] == 'map':
-				sendBundledMsg('/n_map', self._id, name, setter[1])
-
-	def _set(self, name, value):
-		"""
-		Determine how to handle a setattr. Internal names are noops, scalars
-		scalars (including output busses) will be n_set, and everything else
-		results in an n_map.
-		"""
-		if name.startswith('_'):
-			return
-		if type(value) in (int, float):
-			return ('set', value)
-		elif name == 'o' and isinstance(value, KBus):
-			return ('set', value._id)
-		else:
-			# Assume we want to n_map a kr-param to a kr-synth's output
-			# This is a bit tricky since it could be a Synth or a KBus
-			while hasattr(value, 'o'): value = value.o
-			assert isinstance(value, KBus), "%r is not a KBus!" % value
-			return ('map', value._id)
-
-	def __del__(self):
-		# Use sc reference in case it has already quit()
-		sc.send('/n_free', self._id)
-		nodePool.put(self._id)
 
 
 class DecayEnv(object):
 	def __init__(self):
-		self.o = self.decay = Synth('decay', i=kbus0)
+		self.o = self.decay = engine.synth('decay', i=engine.kb0)
 	def trigger(self):
 		thing = self.decay.i
-		with bundle:
-			self.decay.i = Synth('delta', freq=0)
-
+		with engine.bundle:
+			self.decay.i = engine.synth('delta', freq=0)
 
 class Instr(object):
 	def __init__(self):
@@ -99,11 +18,11 @@ class Instr(object):
 		self.voices = collections.deque()
 	def play(self, pitch):
 		now = time.time()
-		with bundle:
+		with engine.bundle:
 			if not self.voices or now < self.voices[0]['time'] + 1.1:
 				#print "creating a new voice"
 				decay = DecayEnv()
-				s = Synth('sine', amp=decay, freq=pitch)
+				s = engine.synth('sine', amp=decay, freq=pitch)
 				voice = {'decay': decay, 's': s}
 			else:
 				voice = self.voices.popleft()
@@ -120,26 +39,11 @@ def main():
 		time.sleep(0.2)
 
 if __name__ == '__main__':
-	sc = scsynth.ScSynth()
-	sc.boot()
-	(send, bundle) = (sc.send, sc.bundled)
-	def sendBundledMsg(*args):
-		with bundle:
-			send(*args)
-
+	engine = scengine.ScEngine()
 	for sd in synthdefs.gen_synthdefs():
-		send('/d_recv', sd.file_data())
-		if getattr(sd, 'output_rate', None) is not None:
-			output_rates[sd.name] = sd.output_rate
-	#scsynth.boot()
-	#send('/dumpOSC', 1); time.sleep(0.1)
-	default = Group(); time.sleep(0.1)
-	kbus0 = KBus()
+		engine.send_synthdef(sd)
+	#engine.send('/dumpOSC', 1); time.sleep(0.1)
 	try:
 		main()
-	except:
-		import traceback
-		traceback.print_exc()
 	finally:
-		sc.quit()
-
+		engine.shutdown()
---
+++ pyon/scengine.py
@@ -0,0 +1,118 @@
+import itertools
+import time
+
+import pool
+import scsynth
+
+output_rates = {}
+
+
+class Group(object):
+	def __init__(self, engine):
+		self._id = engine.node_pool.get()
+		engine.send('/g_new', self._id, 0, 0)
+
+
+class KBus(object):
+	def __init__(self, engine):
+		self.engine = engine 
+		self._id = engine.kbus_pool.get()
+
+	def set(self, value):
+		self.engine.send_bundled_msg('/c_set', self._id, value)
+
+	def __del__(self):
+		self.engine.kbus_pool.put(self._id)
+
+
+class Synth(object):
+	def __init__(self, engine, sd_name, parent, **kwargs):
+		self._engine = engine
+		self._id = engine.node_pool.get()
+		if output_rates.get(sd_name) == 1: # control bus output
+			self._obus = engine.kbus()
+			if 'o' not in kwargs:
+				kwargs['o'] = self._obus
+		with engine.bundle:
+			# setattr ALL the kwargs! But combine the sets and maps for efficiency
+			snew = ['/s_new', sd_name, self._id, 0, parent._id]
+			mapping = {}
+			for (name, value) in kwargs.iteritems():
+				object.__setattr__(self, name, value)
+				setter = self._set(name, value)
+				if setter:
+					if setter[0] == 'set':
+						snew.extend((name, setter[1]))
+					elif setter[0] == 'map':
+						mapping[name] = setter[1]
+			engine.send(*snew)
+			if mapping:
+				engine.send('/n_map', self._id, *itertools.chain( *mapping.items() ))
+
+	def __setattr__(self, name, value):
+		object.__setattr__(self, name, value)
+		setter = self._set(name, value)
+		if setter:
+			if setter[0] == 'set':
+				self._engine.send('/n_set', self._id, name, setter[1])
+			elif setter[0] == 'map':
+				with self._engine.bundle:
+					self._engine.send('/n_map', self._id, name, setter[1])
+
+	def _set(self, name, value):
+		"""
+		Determine how to handle a setattr. Internal names are noops, scalars
+		scalars (including output busses) will be n_set, and everything else
+		results in an n_map.
+		"""
+		if name.startswith('_'):
+			return
+		if type(value) in (int, float):
+			return ('set', value)
+		elif name == 'o' and isinstance(value, KBus):
+			return ('set', value._id)
+		else:
+			# Assume we want to n_map a kr-param to a kr-synth's output
+			# This is a bit tricky since it could be a Synth or a KBus
+			while hasattr(value, 'o'): value = value.o
+			assert isinstance(value, KBus), "%r is not a KBus!" % value
+			return ('map', value._id)
+
+	def __del__(self):
+		# Use sc reference in case it has already quit()
+		self._engine.sc.send('/n_free', self._id)
+		self._engine.node_pool.put(self._id)
+
+
+class ScEngine(object):
+	def __init__(self, sc=None):
+		self.sc = sc or scsynth.ScSynth()
+		self.node_pool = pool.Pool()
+		self.kbus_pool = pool.Pool(0)
+		if not self.sc.booted:
+			self.sc.boot()
+
+		self.send = self.sc.send
+		self.bundle = self.sc.bundled
+		self.default = Group(self)
+		time.sleep(0.1)
+		self.kb0 = KBus(self)
+
+	def kbus(self, value=None):
+		kb = KBus(self)
+		if value is not None:
+			kb.set(value)
+		return kb
+
+	def synth(self, sd_name, parent=None, **kwargs):
+		if not parent:
+			parent = self.default
+		return Synth(self, sd_name, parent, **kwargs)
+
+	def send_synthdef(self, synthdef):
+		self.sc.send('/d_recv', synthdef.file_data())
+		if getattr(synthdef, 'output_rate', None) is not None:
+			output_rates[synthdef.name] = synthdef.output_rate
+
+	def shutdown(self):
+		self.sc.quit()
